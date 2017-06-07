e3e5fde10e06e60bf9a85e15933d824c2d5ef720
Cs_Lab <Cs_Lab@Cs_Lab>
2017-06-07 10:27:07 +0900

    resolve conflict

--- CsProject/src/CsClass.java
+++ CsProject/src/CsClass.java
@@ -4,9 +4,6 @@ public class CsClass
 	public static void main(String[] args)
 	{
 		System.out.println("cs1992");
-	}
-	public static void main(String[] args)
-	{
 		System.out.println("MIndle");
 	}
 }
