1ffae1d02f36898b9524662f9a741619fe918d62
KITRI <KITRI@KITRI-PC>
2017-06-07 14:19:07 +0900

    sum start end

--- CsProject/src/CsClass.java
+++ CsProject/src/CsClass.java
@@ -1,11 +1,21 @@
 
-public class CsClass
-{
-	public static void main(String[] args)
-	{
+public class CsClass {
+	public static void main(String[] args) {
 		System.out.println("cs1992");
 		System.out.println("MIndle");
 		System.out.println("kitri");
 		System.out.println("dkdkdkdk");
+		
+		System.out.println(sum(1, 10));
+	}
+
+	public static int sum(int start, int end) {
+		int result = 0;
+		
+		for(int i = start; i <= end; i++){
+			result += i;
+		}
+		
+		return result;
 	}
 }
